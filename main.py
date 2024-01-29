0df538bb1168b043814dcd300d5f9dc7fa8df780
gbmma <157686088+gbmma@users.noreply.github.com>
2024-01-29 14:53:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 # 웹에서 텍스트 표시
 def main():
-    st.title('경기북부 생계심사') 
+    st.title('경기북부 생계심사 자가진단') 
+    st.header('테스트 중입니다~~')
 
 if __name__ == '__main__' :
     main()
