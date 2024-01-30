20f35920c1ff91984f109e8d581ff3e73a500f2a
gbmma <157686088+gbmma@users.noreply.github.com>
2024-01-30 17:01:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ def main():
 
 if __name__ == '__main__' :
     main()
+
+genre = st.radio(
+     "What's your favorite movie genre",
+     ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+     st.write('You selected comedy.')
+ else:
+     st.write("You didn't select comedy.")
