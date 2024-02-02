fdc3070b152aa588012f24a8a697d7d71ba0396f
gbmma <157686088+gbmma@users.noreply.github.com>
2024-02-02 16:27:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 def main():
     st.title('경기북부병무지청') 
     st.header('생계심사 자가진단')
-    st.subheader('정보관리계에서 테스트 중입니다~')
+    st.subheader('정보관리계에서 테스트 중입니다')
   
 
 if __name__ == '__main__' :
