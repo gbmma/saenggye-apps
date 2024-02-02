6e7a5af1e3d7045329f549aee620c665b63e0694
gbmma <157686088+gbmma@users.noreply.github.com>
2024-02-02 17:17:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,19 @@ st.markdown(
 
 """
 )
+openai.api_key = st.secrets["openai_api_key"]
+
 def openai_create(prompt):
+
+    response = openai.ChatCompletion.create(
+    model=st.secrets["openai_api_model"],
+    messages=[{"role": "system", "content": st.secrets["prompt_canvas"]["prompt_system"]}, 
+              {"role": "user", "content": prompt}], 
+    temperature=float(st.secrets["openai_api_temp"]), 
+    max_tokens=int(st.secrets["openai_api_maxtok"]),
+    frequency_penalty=int(st.secrets["openai_api_freqp"]),
+    presence_penalty=float(st.secrets["openai_api_presp"]), 
+    stop = None
+    )
+
+    return response['choices'][0]['message']['content']
