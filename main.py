76bc980c990fed04a3d574090537a33e9c28eb8d
gbmma <157686088+gbmma@users.noreply.github.com>
2024-02-13 13:52:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
-st.sidebar.title('생계심사 자가진단 테스트')
-
+st.sidebar.title('🌸생계심사 자가진단 테스트🌸')
+select_species = st.sidebar.selectbox(
+    '확인하고 싶은 종을 선택하세요',
+    ['setosa','versicolor','virginica']
+)
 
 st.set_page_config(
     page_title="경기북부병무지청 생계심사 자가진단",
