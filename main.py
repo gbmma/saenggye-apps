5cd0f8a796b8a8ecb70b75092693054b2807ba91
gbmma <157686088+gbmma@users.noreply.github.com>
2024-02-13 14:21:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ if __name__ == '__main__':
     st.title("생계곤란사유 병역감면 진단")
 
     # 사용자로부터 다양한 정보를 입력받습니다.
+    age = st.slider("나이를 입력하세요", min_value=18, max_value=40, value=20)
+    military_service = st.selectbox("군 복무 경력이 있습니까?", ["없음", "있음"])
     income = st.number_input("가계 소득을 입력하세요 (원)", min_value=0, step=1000000, value=0)
     family_members = st.slider("가족 수를 입력하세요", min_value=1, max_value=10, value=1)
-    health_status = st.selectbox("건강 상태를 선택하세요", ["건강함", "질병/장해 있음"])
-    military_service = st.selectbox("군 복무 경력이 있습니까?", ["없음", "있음"])
+    health_status = st.selectbox("건강 상태를 선택하세요", ["건강함", "질병/장해 있음"]) 
 
     # ""결과 확인하기" 버튼을 생성합니다.
     if st.button("결과 확인하기"):
