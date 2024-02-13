f5e03c1be6f89d17aae90f12893c0c337c9d66f2
gbmma <157686088+gbmma@users.noreply.github.com>
2024-02-13 13:50:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
-menu = ['이미지 업로드', 'csv 업로드', 'About']
+
+st.sidebar.title('생계심사 자가진단 테스트')
+
+
 st.set_page_config(
     page_title="경기북부병무지청 생계심사 자가진단",
     page_icon=":pickup_truck:",
@@ -7,8 +10,7 @@ st.set_page_config(
 )
 
 st.write("# 경기북부병무지청 생계 자가진단")
-
-st.sidebar.success("여ㅇ긴")
+ 
 
 st.markdown(
     """##### 테스트중!
