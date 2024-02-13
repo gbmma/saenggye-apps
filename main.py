ad90cf1407fee5be8f8b34895739b05d256e9043
gbmma <157686088+gbmma@users.noreply.github.com>
2024-02-13 14:00:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import streamlit as st
-
-st.sidebar.title('생계유지곤란사유 병역감면원')
-select_species = st.sidebar.selectbox(
-    '확인하고 싶은 사항을 선택하세요',
-    ['자가진단 참고사항','병역감면 제도 개요','자가진단 시작하기']
-)
-
+import json
+ 
 st.set_page_config(
     page_title="경기북부병무지청 생계심사 자가진단",
     page_icon=":pickup_truck:",
