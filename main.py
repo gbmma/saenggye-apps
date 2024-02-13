a97b9a168904e321bcf0aad10e6459b362f150c9
gbmma <157686088+gbmma@users.noreply.github.com>
2024-02-13 14:04:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import streamlit as st   
 
-import os
-import json
-
-import openai
-
-path = os.path.dirname(__file__)
-
-# Load translations from JSON file
-with open(path+"/../translations.json") as f:
-    transl = json.load(f) 
- 
 st.set_page_config(
     page_title="경기북부병무지청 생계심사 자가진단",
     page_icon=":pickup_truck:",
